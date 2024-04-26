a27b4e21f9607acdf3dbc4f14445a06b4a299b2a
Reginald Sahil Chand <Developer.ReginaldSahilChand@gmail.com>
2024-04-27 04:52:14 +1200

    Add user welcome text and date

--- Desktop Setup/Operating System/rsc_ubuntu.sh
+++ Desktop Setup/Operating System/rsc_ubuntu.sh
@@ -1,1 +1,12 @@
+# This file has been created by Reginald Sahil Chand on Sat April 27 2024.
+# Copyright @ Reginald Sahil Chand | RSC Solutions 2024. All rights reserved.
+
+# Greetings.
+echo "Reginald Sahil Chand Welcomes You (WELCOME)\n"
+date
+echo "\nYou are\n"
+whoami
+echo "\n"
+
+
 
